5cd62ef99ec61c42c1f02481c8223436e1dbc5f5
Abdul-asa <bigtulahi1@gmail.com>
2025-08-08 00:41:30 +0100

    done

--- components/provider.tsx
+++ components/provider.tsx
@@ -4,9 +4,8 @@ import { useAtomsDebugValue } from "jotai-devtools";
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   const DebugAtoms = () => {
-    if (process.env.NODE_ENV === "development") {
-      useAtomsDebugValue();
-    }
+    useAtomsDebugValue();
+
     return null;
   };
   return (
